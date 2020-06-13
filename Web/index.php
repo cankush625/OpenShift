c67655ddb964c19241a6649d33169c35203b2641
Ankush Chavan <cankush625@gmail.com>
2020-06-13 20:28:15 +0530

    Version 2
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- Web/index.php
+++ Web/index.php
@@ -3,7 +3,7 @@
     <title>Welcome to the OpenShift!</title>
   </head>
   <body>
-    Project launched using OpenShift<br>
+    Project launched using OpenShift!<br>
     <img src="OpenShiftLogo.png" alt="OpenShift Logo">
   </body>
 </html>
