93f80326fa9d45db78d7674d57280d8fffed0f81
Ankush Chavan <cankush625@gmail.com>
2020-06-13 20:12:41 +0530

    OpenShift Logo added to the project
    Signed-off-by: Ankush Chavan <cankush625@gmail.com>

--- Web/index.php
+++ Web/index.php
@@ -3,6 +3,7 @@
     <title>Welcome to the OpenShift!</title>
   </head>
   <body>
-    Project launched using OpenShift
+    Project launched using OpenShift<br>
+    <img src="OpenShiftLogo.png" alt="OpenShift Logo">
   </body>
 </html>
